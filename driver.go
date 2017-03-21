246865b7d16893b1f1ea6a238b5b176c79bcd85d
reachlin <reachlin@gmail.com>
2017-03-21 17:43:56 +0800

    return

--- driver.go
+++ driver.go
@@ -13,33 +13,33 @@ func NewDriver(root string) dvd4sfDriver {
 }
 
 func (d *dvd4sfDriver) Create(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) List(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Get(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Remove(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Path(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Mount(req volume.MountRequest) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Unmount(req volume.UnmountRequest) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
 
 func (d *dvd4sfDriver) Capabilities(req volume.Request) volume.Response {
-  volume.Response{Err: "not implemented"}
+  return volume.Response{Err: "not implemented"}
 }
